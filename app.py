c4921c1d5881446f90949c203359c1bc31b408db
JackSpades17 <drozhzhin.1993@mail.ru>
2024-10-13 15:39:37 +0300

    new version 1.1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, Nastya with sex ass!</h1>'
+    return '<h1>Hello, Nastya you are my queen!</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=1717)
