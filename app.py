8f5f0395125fcb43aebd3fff031954597b6d2f43
JackSpades17 <drozhzhin.1993@mail.ru>
2024-12-01 17:08:37 +0300

    update services

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, Nastya is a COOKIE WIFE!!!</h1>'
+    return '<h1>Hello, Nastya is a WONDERFUL WIFE!!!</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=1717)
