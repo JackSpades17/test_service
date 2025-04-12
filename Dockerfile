9848432f2b045287cda7503a54044e557647daac
JackSpades17 <drozhzhin.1993@mail.ru>
2025-04-12 13:12:29 +0300

    new_psql

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /opt/app
 COPY requirements.txt .
 COPY app.py .
 RUN pip3 install -r /opt/app/requirements.txt
+RUN apt install postgresql-client-common
 EXPOSE 1717
 CMD python3 ./app.py
 
--- app.py
+++ app.py
@@ -1,11 +1,43 @@
 from flask import Flask
-
+import psycopg2
+import argv
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return '<h1>Hello, Nastya is a LOVA GIRL!!!</h1>'
+
+# def connect_to_db():
+#     conn = psycopg2.connect (
+#         #database = "postgres",
+#         #user = "test_user",
+#         #password = "1111",
+#         host = "192.168.49.2",
+#         port = "5432"
+#     )   
+#     return conn
+
+@app.route('/info')
+def get_price():
+    with psycopg2.connect(host = "192.168.49.2", port = "5432") as conn:
+        cur = conn.cursor()
+        cur.execute('''select * from info_girl;''')
+        sets = cur.fetchone()
+    print(sets)
+
+    return f'<h1>Age:{sets[0]}</h1>'
+
+# @app.route('/update')
+# def update_price():
+#     cur.execute('''select * from count_update order by count desc;''')
+#     sets = cur.fetchone()
+#     new_item = int(sets[0]) + 1
+#     cur.execute(f'''insert into count_update values('{new_item}');''')
+#     con.commit()
+#     cur.execute('''select * from count_update order by count desc;''')
+#     sets = cur.fetchone()
+#     return f'<h1>Congratulations!Обновлено! BIG is {sets[0]}!</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=1717)
